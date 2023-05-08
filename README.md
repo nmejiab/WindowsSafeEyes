bdc9d40a4e979094686a6fa2854fcd07333238ce
unknown <nmejiab@unal.edu.co>
2023-05-08 17:46:54 -0500

    feat: Hide application icon - some errors at reopen

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # WindowsSafeEyes
 Protect your eyes from eye strain using this simple and beautiful, yet extensible break reminder on Windows
+
+# Requierements
+To get the repository could be useful get GIT for windows it could be gotten in [this link](https://git-scm.com/downloads)
+
+
+# Build
+First of all you must clone the repository in your windows10 machine to get the project
+'''
+git clone https://github.com/nmejiab/WindowsSafeEyes.git
+'''
+After that set on the project folder to get the requirements
+'''
+pip install -r requirements.txt
+'''
+# Run
+In the root project folder:
+'''
+py .\main.py
+'''
+
+![Hello i'm under the water](https://media.tenor.com/AjFh9zcCyDMAAAAd/hello-i-am-under-the-water.gif)
--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import tkinter as tk
+from PIL import Image
+import pystray
+import time
 
-def minimizar():
-    root.iconify()
+icon = Image.open('icon.png')
+
+def show_window():
+    root.deiconify()
+
+def quit_app():
+    root.quit()
+
+def minimize():
+    root.withdraw() # Ocultar la ventana de tkinter
+    menu = pystray.Menu(pystray.MenuItem('Abrir', show_window), pystray.MenuItem('Salir', quit_app))
+    tray_icon = pystray.Icon('nombre_icono', icon,  'Mensaje de tooltip', menu)
+    tray_icon.run()
+
 
 root = tk.Tk()
 root.attributes('-fullscreen', True)
@@ -9,7 +24,8 @@ root.configure(background='black')
 label = tk.Label(root, text="Safe eyes for windows is coming!", font=("Helvetica", 27), fg="white", bg="black")
 label.pack(expand=True)
 label.place(relx = 0.5, rely = 0.45, anchor = 'center')
-button = tk.Button(root, text = "Skip", command = minimizar, font = ("Helvetica", 15))
+button = tk.Button(root, text = "Skip", command = minimize, font = ("Helvetica", 15))
 button.pack()
 button.place(relx = 0.5, rely = 0.55, anchor = 'center')
+
 root.mainloop()
