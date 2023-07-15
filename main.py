e5bb1b6e0f7fd2adf66bccf7aca2b694b3e4fb8e
unknown <nmejiab@unal.edu.co>
2023-07-15 02:37:26 -0500

    cleaning code

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from PIL import Image
 import pystray
-import time
 
 icon = Image.open('icon.png')
 
@@ -12,20 +11,21 @@ def quit_app():
     root.quit()
 
 def minimize():
-    root.withdraw() # Ocultar la ventana de tkinter
-    menu = pystray.Menu(pystray.MenuItem('Abrir', show_window), pystray.MenuItem('Salir', quit_app))
-    tray_icon = pystray.Icon('nombre_icono', icon,  'Mensaje de tooltip', menu)
+    root.withdraw() # Hide the tkinter window
+    menu = pystray.Menu(pystray.MenuItem('Open', show_window), pystray.MenuItem('Quit', quit_app))
+    tray_icon = pystray.Icon('icon_name', icon, 'Tooltip Message', menu)
     tray_icon.run()
-
 
 root = tk.Tk()
 root.attributes('-fullscreen', True)
 root.configure(background='black')
-label = tk.Label(root, text="Safe eyes for windows is coming!", font=("Helvetica", 27), fg="white", bg="black")
+
+label = tk.Label(root, text="Safe Eyes for Windows is Coming!", font=("Helvetica", 27), fg="white", bg="black")
 label.pack(expand=True)
-label.place(relx = 0.5, rely = 0.45, anchor = 'center')
-button = tk.Button(root, text = "Skip", command = minimize, font = ("Helvetica", 15))
+label.place(relx=0.5, rely=0.45, anchor='center')
+
+button = tk.Button(root, text="Skip", command=minimize, font=("Helvetica", 15))
 button.pack()
-button.place(relx = 0.5, rely = 0.55, anchor = 'center')
+button.place(relx=0.5, rely=0.55, anchor='center')
 
 root.mainloop()
